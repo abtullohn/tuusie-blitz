bf0bca0244ef0b9fe5dd0f44f8e57bcf7382ea84
abtullohn <abtullohn@gmail.com>
2022-10-09 19:42:07 -0400

    questions and response

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+
+def homePage(request):
+    context ={
+          'title': 'World'
+    }
+    return render(request,'homepage.html', context)
