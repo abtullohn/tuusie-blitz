77fd02c13231b3c051b39934305205f46d8bda9c
abtullohn <abtullohn@gmail.com>
2022-10-09 20:13:19 -0400

    homepage

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here
 
 def homePage(request):
+    questions = Question.objects.all().order_by('-created_at')
     context ={
-          'title': 'World'
+          'questions': questions
     }
     return render(request,'homepage.html', context)
