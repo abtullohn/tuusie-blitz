16ac9cc4e13f64da55becbb29a40824e2abe5ff3
abtullohn <abtullohn@gmail.com>
2022-10-10 15:06:16 -0400

    login page

--- main_app/views.py
+++ main_app/views.py
@@ -27,22 +27,22 @@ def registerPage(request):
     return render(request,'register.html', context)
 
 
-# def loginPage(request):
-#     form = LoginForm()
+def loginPage(request):
+    form = LoginForm()
 
-#     if request.method == 'POST':
-#         try:
-#             form = LoginForm(data=request.POST)
-#             if form.is_valid():
-#                 user=form.get_user()
-#                 login(request, user)
-#                 return redirect('index')
-#         except Exception as e:
-#             print(e)
-#             raise
+    if request.method == 'POST':
+        try:
+            form = LoginForm(data=request.POST)
+            if form.is_valid():
+                user=form.get_user()
+                login(request, user)
+                return redirect('index')
+        except Exception as e:
+            print(e)
+            raise
 
-#     context ={'form': form}
-#     return render(request, 'login.html', context)
+    context ={'form': form}
+    return render(request, 'login.html', context)
 
 def homePage(request):
     questions = Question.objects.all().order_by('-created_at')
