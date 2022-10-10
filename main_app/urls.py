8188e45a17500ad32437cfcb4050851fe532d825
abtullohn <abtullohn@gmail.com>
2022-10-10 11:51:48 -0400

    styling for homepage

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homePage, name ='index')
+    path('register', views.registerPage, name='register'),
+    path('', views.homePage, name ='index'),
+    path('question/<int:id>', views.questionPage, name='question')
 ]
