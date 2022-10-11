c7145151eb4b3b2b84a8592067eaf61eb5411317
abtullohn <abtullohn@gmail.com>
2022-10-11 11:50:30 -0400

    working on reply button

--- main_app/static/css/js/main.js
+++ main_app/static/css/js/main.js
@@ -1,6 +1,6 @@
 function handleReplyButton(responseId){
     const replyFormContainer = document.getElementById(`reply-form-container-${responseId}`);
     if(replyFormContainer){
-        // replyFormContainer.className = 'reply-form-container enabled'
+        replyFormContainer.className = 'reply-form-container enabled'
     }
 }
