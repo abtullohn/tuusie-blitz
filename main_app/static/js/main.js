0bd1e304a2605bd0d6aa7072004e67b2dcb920f6
abtullohn <abtullohn@gmail.com>
2022-10-11 13:43:52 -0400

    reply in a response

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,12 +1,13 @@
+console.log('main.js loaded')
 function handleReplyButton(responseId) {
     const replyFormContainer = document.getElementById(`reply-form-container-${responseId}`);
     if (replyFormContainer) {
       replyFormContainer.className = 'reply-form-container enabled'
     }
   }
-
-function handleCancelReply(responseId){
-
-}
-
+  
+  function handleCancelReply(responseId) {
+    
+    }
+  
 // document.querySelector('.reply-button').addEventListener('click', handleReplyButton)
