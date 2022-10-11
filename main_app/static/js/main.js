54f6e678ab6ea38e4bfb130801290626375116b5
abtullohn <abtullohn@gmail.com>
2022-10-11 13:49:35 -0400

    cancel reply form

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -7,6 +7,10 @@ function handleReplyButton(responseId) {
   }
   
   function handleCancelReply(responseId) {
+    const replyFormContainer = document.getElementById(`reply-form-container-${responseId}`);
+    if (replyFormContainer) {
+      replyFormContainer.className = 'reply-form-container'
+    }
     
     }
   
